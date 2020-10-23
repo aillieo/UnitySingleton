03eb27b4ca3e99fad06200eddcf888128a3635be
aillieo <aillieo@163.com>
2020-10-23 20:33:16 +0800

    add singleton scriptableobject

--- Assets/UnitySingleton/Scripts/Singleton.cs
+++ Assets/UnitySingleton/Scripts/Singleton.cs
@@ -1,6 +1,6 @@
 namespace AillieoUtils
 {
-    public class Singleton<T> where T : class, new()
+    public abstract class Singleton<T> where T : class, new()
     {
         private static T m_instance = null;
 
