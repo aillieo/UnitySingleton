7f3126addb7375fa8d0b2e7b0c5675d399c0222b
aillieo <aillieo@163.com>
2023-02-05 16:15:08 +0800

    update

--- Scripts/SingletonThreadLocal.cs
+++ Scripts/SingletonThreadLocal.cs
@@ -6,6 +6,7 @@
 
 namespace AillieoUtils
 {
+    using System;
     using System.Threading;
 
     /// <summary>
@@ -15,27 +16,65 @@ namespace AillieoUtils
     public abstract class SingletonThreadLocal<T>
         where T : SingletonThreadLocal<T>, new()
     {
-        private static ThreadLocal<T> instance = new ThreadLocal<T>(() => new T());
+        private static ThreadLocal<T> instance = new ThreadLocal<T>();
 
         /// <summary>
-        /// Gets a value indicating whether the instance is created.
+        /// Gets a value indicating whether the instance is created for current thread.
         /// </summary>
         public static bool HasInstance
         {
             get
             {
-                return instance.IsValueCreated;
+                return instance != null && instance.IsValueCreated && instance.Value != null;
             }
         }
 
         /// <summary>
-        /// Gets the instance of the singleton.
+        /// Gets the instance of the singleton for current thread.
         /// </summary>
         public static T Instance
         {
             get
             {
+                if (instance == null)
+                {
+                    throw new InvalidOperationException($"All instances of {typeof(T)} were destroyed.");
+                }
+
+                if (!instance.IsValueCreated)
+                {
+                    instance.Value = new T();
+                }
+
                 return instance.Value;
+            }
+        }
+
+        /// <summary>
+        /// Destroy the instance for current thread.
+        /// </summary>
+        public static void Destroy()
+        {
+            if (instance.IsValueCreated)
+            {
+                instance.Value = null;
+            }
+        }
+
+        /// <summary>
+        /// Destroy the instances for all threads.
+        /// </summary>
+        public static void DestroyAll()
+        {
+            if (instance == null)
+            {
+                return;
+            }
+
+            var instanceToDispose = Interlocked.Exchange(ref instance, null);
+            if (instanceToDispose != null)
+            {
+                instanceToDispose.Dispose();
             }
         }
     }
